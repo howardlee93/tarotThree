6682d687839f6f0b270c7579a192121affe0321f
Howard <howardl93@yahoo.com>
2021-01-31 20:41:09 -0800

    stylistic changes

--- components/meaning_card.js
+++ components/meaning_card.js
@@ -6,7 +6,7 @@ import styles from '../styles/meaning.module.css';
 import Image from 'next/image';
 
 
-const Meaning_Card = ({card, cardDes, cardDir}) =>{
+const Meaning_Card = ({card, cardDes, cardDir}) =>{ //cardDir
 
     const [toggled, setToggled] = useState(true);
 
@@ -16,6 +16,9 @@ const Meaning_Card = ({card, cardDes, cardDir}) =>{
          
         setToggled(!toggled);
     }
+
+    // let cardDir = ["meaning_up", "meaning_rev"][ Math.floor(Math.random()*2 +1 )];
+
     return(
         <div className={styles.container}>
             <Image src={imgSrc} alt="me" width="200%" height="333%" onClick={cardClick}
@@ -29,7 +32,7 @@ const Meaning_Card = ({card, cardDes, cardDir}) =>{
                 <div>
                     <h1>{card.name}</h1>
                     {cardDir == "meaning_up" ? <p>up</p> : <p>reversed</p>}
-                    <p>{cardDes}</p>
+                    <p>{cardDes}</p> 
             </div>}
 
         </div>
--- components/spread_layout.js
+++ components/spread_layout.js
@@ -31,12 +31,12 @@ export default function Spread({children}){ // will need to pass props in here
     }
 
     const meaningSpread = cards.map((card,i) =>
-      <Meaning_Card card={card} key={i} cardDir={dir} cardDes={card[dir]}/>
+      <Meaning_Card card={card} key={i} cardDir={dir}  cardDes={card[dir]}/> 
     )
       
     return(
         <div className={styles.main}> 
-          <button onClick={cardClick}>Shuffle the cards</button>
+          <p onClick={cardClick}>Shuffle the cards</p>
           <p> Click the cards to reveal your fate</p>
 
             <article className={styles.reading}>
